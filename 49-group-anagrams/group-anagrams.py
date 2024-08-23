86fa9cc9a33364aa4f4d274d1aec0c0c6401a0f1
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-08-22 20:37:14 -0400

    Time: 84 ms (81.72%) | Memory: 20.7 MB (37.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # time complexity
+        # O(N)      length of strs
+        # O(K)      maximum length of string in str
+        # O(KlogK)  sorted each string
+
+        # O(NK)
         ans = collections.defaultdict(list)
         for s in strs:
             ans[tuple(sorted(s))].append(s)
