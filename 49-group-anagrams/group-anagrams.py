48dea28dfb601b81f182cb72b82c5d692fffae13
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-08-25 02:27:40 -0400

    Time: 94 ms (38.32%) | Memory: 21.9 MB (26.14%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # time complexity
-        # O(N)      length of strs
-        # O(K)      maximum length of string in str
-        # O(KlogK)  sorted each string
-
-        # O(NK)
         ans = collections.defaultdict(list)
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
         return ans.values()
