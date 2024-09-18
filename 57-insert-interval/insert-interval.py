9481299ff4dbc82c625e81face90a42ea688755c
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-09-18 16:40:14 -0400

    Time: 74 ms (63.99%) | Memory: 20 MB (15.88%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,22 +1,27 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals)
-        i = 0
-        res = []
+        # i want to store the length of list
+        # next use the two variable to store the front and the back
+        # until the end
 
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
+        length = len(intervals)
+        i = 0
+        result = []
+
+        while i != length and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
             i += 1
-        
-        while i < n and newInterval[1] >= intervals[i][0]:
+        while i < length and newInterval[1] >= intervals[i][0]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
             i += 1
-        res.append(newInterval)
-
-        while i < n:
-            res.append(intervals[i])
+        result.append(newInterval)
+        while i != length and intervals[i][0] > newInterval[0]:
+            result.append(intervals[i])
             i += 1
-        return res
+        return result
 
+            
+        
 
+                
