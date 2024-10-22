a6a9ec1c5d75b8c0fd58c709e0e0c0f4ba9bbbd2
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-10-22 05:33:04 -0400

    Time: 4 ms (97.35%) | Memory: 17.8 MB (41.79%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Hashtable
+
+        # There is no edge case in this problem
+        
+        # create a dict for visted 
         visited = {}
+        # will create for loop with length of nums
         for i in range(len(nums)):
             if target - nums[i] in visited:
                 return [i, visited[target - nums[i]]]
             visited[nums[i]] = i
-        hashMap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashMap:
-                return [hashMap[complement], i]
-            hashMap[nums[i]] = i
