3fcddffb53f90e5a69721a75a6abdf1c81c40ec1
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-10-22 18:30:33 -0400

    Time: 0 ms (100.00%) | Memory: 16.8 MB (7.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # this is db problem because we can save all the pass proabilitys
-        
-        # time complexity: O(N^2)
         if n == 1:
             return 1
-        if n == 2:
-            return 2
-        prev1, prev2 = 1, 2
-        for _ in range(3, n+1):
-            curr = prev1 + prev2
-            prev1 = prev2
-            prev2 = curr
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+
         
-        return curr
+        
