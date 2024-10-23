63b9a37382738b80124c81098a55d97c84cd1fb2
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-10-22 20:05:27 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.12%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        if len(s) < 2:
-            return len(s)
-        char_dict = {}
-        result = 0
+        nums = {}
+        maxlength = 0
         for char in s:
-            if char not in char_dict:
-                char_dict[char] = 1
+            if char not in nums:
+                nums[char] = 1
             else:
-                del char_dict[char]
-                result += 2
-            print(char_dict)
-        if char_dict:
-            return result + 1
-        return result
-
+                del nums[char]
+                maxlength += 2
+                
+        return maxlength + 1 if nums else maxlength 
 
         
