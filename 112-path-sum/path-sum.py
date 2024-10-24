25bf14f45395f783c0ac7a77c15e9e224308d5aa
Minp45 <72828584+Minp45@users.noreply.github.com>
2024-10-24 03:40:56 -0400

    Time: 3 ms (94.37%) | Memory: 17.6 MB (8.94%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,14 +6,12 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root:
-            return False
         def dfs(node, targetSum):
             if not node:
-                return
+                return False
             if node.val == targetSum and not node.left and not node.right:
                 return True
             return dfs(node.left, targetSum - node.val) or dfs(node.right, targetSum - node.val)
         
-        return True if dfs(root, targetSum) else False
+        return dfs(root, targetSum)
         
